17d88455c2f2b9f4ff40a6a00db5a68ab5677f86
Chandru Vinayagam <rajuashwin1410@gmail.com>
2024-08-08 15:16:34 +0530

    blocked mobile view

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,41 +1,40 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip({
-     html: true
+    html: true,
   });
 });
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Mobile screen block 
+  if (window.innerWidth <= 768) {
+    $("#mobileModal").modal({
+      show: false,
+      backdrop: "static",
+      keyboard: false,
     });
+    $("#mobileModal").modal("show");
+    $("#desktop").empty();
+  }
 
+  // Activate Bootstrap scrollspy on the main nav element
+  const sideNav = document.body.querySelector("#sideNav");
+  if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#sideNav",
+      offset: 74,
+    });
+  }
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
 });
-
-$(function () {
-    if(window.innerWidth <= 768){
-        $("#mobileModal").modal({show: false,backdrop: 'static',keyboard: false});
-        $("#mobileModal").modal("show");   
-        $("#desktop").empty();
-    }
-});
-
-
